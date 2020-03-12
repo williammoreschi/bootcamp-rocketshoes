55ad805e4b6c15304604c38c063c77a2092dbb80
william moreschi <williammoreschi@gmail.com>
2020-03-12 05:20:16 -0300

    Criado/Configurado todo esquema para que o redux funciona na aplicação

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 import Header from './components/Header';
 
+import store from './store';
+
 export default function src() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Header />
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Header />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
